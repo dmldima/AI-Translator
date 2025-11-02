e63114b7d570b4c149b254d4f186ca392177c5f9
dmldima <dmldima@gmail.com>
2025-11-02 22:31:48 +0100

    Update __init__.py

--- src/utils/__init__.py
+++ src/utils/__init__.py
@@ -2,9 +2,11 @@
 from .config_manager import ConfigManager, get_config_manager, get_config
 from .logger import setup_logging, get_logger
 from .validators import validate_document, validate_language_pair
+from .text_normalizer import CachedTextNormalizer, normalize, norm_key  # NEW
 
 __all__ = [
     "ConfigManager", "get_config_manager", "get_config",
     "setup_logging", "get_logger",
     "validate_document", "validate_language_pair",
+    "CachedTextNormalizer", "normalize", "norm_key",  # NEW
 ]
